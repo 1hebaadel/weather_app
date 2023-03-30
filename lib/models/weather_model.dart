e6a9cbd924fdc7d4edc02c3bf18e79bb7d706e4e
1hebaadel <hebaadelahmedmohammed@gmail.com>
2023-03-30 23:28:58 +0200

    Commit model

---
+++ lib/models/weather_model.dart
@@ -0,0 +1,255 @@
+class WeatherModel {
+  Location? location;
+  Current? current;
+  Forecast? forecast;
+
+  WeatherModel.fromJson(Map<String, dynamic> json) {
+    location = json['location'] != null ? new Location.fromJson(json['location']) : null;
+    current = json['current'] != null ? new Current.fromJson(json['current']) : null;
+    forecast = json['forecast'] != null ? new Forecast.fromJson(json['forecast']) : null;
+  }
+}
+
+class Location {
+  String? name;
+  String? region;
+  String? country;
+  double? lat;
+  double? lon;
+  String? tzId;
+  int? localtimeEpoch;
+  String? localtime;
+
+  Location.fromJson(Map<String, dynamic> json) {
+    name = json['name'];
+    region = json['region'];
+    country = json['country'];
+    lat = json['lat'];
+    lon = json['lon'];
+    tzId = json['tz_id'];
+    localtimeEpoch = json['localtime_epoch'];
+    localtime = json['localtime'];
+  }
+
+}
+
+class Current {
+  int? lastUpdatedEpoch;
+  String? lastUpdated;
+  int? tempC;
+  double? tempF;
+  int? isDay;
+  Condition? condition;
+  double? windMph;
+  double? windKph;
+  int? windDegree;
+  String? windDir;
+  int? pressureMb;
+  double? pressureIn;
+  int? precipMm;
+  int? precipIn;
+  int? humidity;
+  int? cloud;
+  double? feelslikeC;
+  double? feelslikeF;
+  int? visKm;
+  int? visMiles;
+  int? uv;
+  double? gustMph;
+  double? gustKph;
+
+  Current.fromJson(Map<String, dynamic> json) {
+    lastUpdatedEpoch = json['last_updated_epoch'];
+    lastUpdated = json['last_updated'];
+    tempC = json['temp_c'];
+    tempF = json['temp_f'];
+    isDay = json['is_day'];
+    condition = json['condition'] != null ? new Condition.fromJson(json['condition']) : null;
+    windMph = json['wind_mph'];
+    windKph = json['wind_kph'];
+    windDegree = json['wind_degree'];
+    windDir = json['wind_dir'];
+    pressureMb = json['pressure_mb'];
+    pressureIn = json['pressure_in'];
+    precipMm = json['precip_mm'];
+    precipIn = json['precip_in'];
+    humidity = json['humidity'];
+    cloud = json['cloud'];
+    feelslikeC = json['feelslike_c'];
+    feelslikeF = json['feelslike_f'];
+    visKm = json['vis_km'];
+    visMiles = json['vis_miles'];
+    uv = json['uv'];
+    gustMph = json['gust_mph'];
+    gustKph = json['gust_kph'];
+  }
+
+}
+
+class Condition {
+  String? text;
+  String? icon;
+  int? code;
+
+  Condition({this.text, this.icon, this.code});
+
+  Condition.fromJson(Map<String, dynamic> json) {
+    text = json['text'];
+    icon = json['icon'];
+    code = json['code'];
+  }
+}
+
+class Forecast {
+  List<Forecastday> forecastday = [];
+
+  Forecast.fromJson(Map<String, dynamic> json) {
+    if (json['forecastday'] != null) {
+      forecastday = <Forecastday>[];
+      json['forecastday'].forEach((v) {
+        forecastday!.add(new Forecastday.fromJson(v));
+      });
+    }
+  }
+
+}
+
+class Forecastday {
+  String? date;
+  int? dateEpoch;
+  Day? day;
+  List<Hour>? hour;
+
+  Forecastday.fromJson(Map<String, dynamic> json) {
+    date = json['date'];
+    dateEpoch = json['date_epoch'];
+    day = json['day'] != null ? new Day.fromJson(json['day']) : null;
+    if (json['hour'] != null) {
+      hour = <Hour>[];
+      json['hour'].forEach((v) {
+        hour!.add(new Hour.fromJson(v));
+      });
+    }
+  }
+
+}
+
+class Day {
+  double? maxtempC;
+  double? maxtempF;
+  double? mintempC;
+  double? mintempF;
+  double? avgtempC;
+  double? avgtempF;
+  double? maxwindMph;
+  double? maxwindKph;
+  int? totalprecipMm;
+  int? totalprecipIn;
+  int? totalsnowCm;
+  int? avgvisKm;
+  int? avgvisMiles;
+  int? avghumidity;
+  int? dailyWillItRain;
+  int? dailyChanceOfRain;
+  int? dailyWillItSnow;
+  int? dailyChanceOfSnow;
+  Condition? condition;
+  int? uv;
+
+  Day.fromJson(Map<String, dynamic> json) {
+    maxtempC = json['maxtemp_c'];
+    maxtempF = json['maxtemp_f'];
+    mintempC = json['mintemp_c'];
+    mintempF = json['mintemp_f'];
+    avgtempC = json['avgtemp_c'];
+    avgtempF = json['avgtemp_f'];
+    maxwindMph = json['maxwind_mph'];
+    maxwindKph = json['maxwind_kph'];
+    totalprecipMm = json['totalprecip_mm'];
+    totalprecipIn = json['totalprecip_in'];
+    totalsnowCm = json['totalsnow_cm'];
+    avgvisKm = json['avgvis_km'];
+    avgvisMiles = json['avgvis_miles'];
+    avghumidity = json['avghumidity'];
+    dailyWillItRain = json['daily_will_it_rain'];
+    dailyChanceOfRain = json['daily_chance_of_rain'];
+    dailyWillItSnow = json['daily_will_it_snow'];
+    dailyChanceOfSnow = json['daily_chance_of_snow'];
+    condition = json['condition'] != null ? new Condition.fromJson(json['condition']) : null;
+    uv = json['uv'];
+  }
+
+}
+
+class Hour {
+  int? timeEpoch;
+  String? time;
+  double? tempC;
+  double? tempF;
+  int? isDay;
+  Condition? condition;
+  double? windMph;
+  double? windKph;
+  int? windDegree;
+  String? windDir;
+  int? pressureMb;
+  double? pressureIn;
+  int? precipMm;
+  int? precipIn;
+  int? humidity;
+  int? cloud;
+  double? feelslikeC;
+  double? feelslikeF;
+  double? windchillC;
+  double? windchillF;
+  double? heatindexC;
+  double? heatindexF;
+  double? dewpointC;
+  double? dewpointF;
+  int? willItRain;
+  int? chanceOfRain;
+  int? willItSnow;
+  int? chanceOfSnow;
+  int? visKm;
+  int? visMiles;
+  double? gustMph;
+  double? gustKph;
+  int? uv;
+
+  Hour.fromJson(Map<String, dynamic> json) {
+    timeEpoch = json['time_epoch'];
+    time = json['time'];
+    tempC = json['temp_c'];
+    tempF = json['temp_f'];
+    isDay = json['is_day'];
+    condition = json['condition'] != null ? new Condition.fromJson(json['condition']) : null;
+    windMph = json['wind_mph'];
+    windKph = json['wind_kph'];
+    windDegree = json['wind_degree'];
+    windDir = json['wind_dir'];
+    pressureMb = json['pressure_mb'];
+    pressureIn = json['pressure_in'];
+    precipMm = json['precip_mm'];
+    precipIn = json['precip_in'];
+    humidity = json['humidity'];
+    cloud = json['cloud'];
+    feelslikeC = json['feelslike_c'];
+    feelslikeF = json['feelslike_f'];
+    windchillC = json['windchill_c'];
+    windchillF = json['windchill_f'];
+    heatindexC = json['heatindex_c'];
+    heatindexF = json['heatindex_f'];
+    dewpointC = json['dewpoint_c'];
+    dewpointF = json['dewpoint_f'];
+    willItRain = json['will_it_rain'];
+    chanceOfRain = json['chance_of_rain'];
+    willItSnow = json['will_it_snow'];
+    chanceOfSnow = json['chance_of_snow'];
+    visKm = json['vis_km'];
+    visMiles = json['vis_miles'];
+    gustMph = json['gust_mph'];
+    gustKph = json['gust_kph'];
+    uv = json['uv'];
+  }
+
+}
